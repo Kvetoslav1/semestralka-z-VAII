a0bdaeca0a710eb16c5bb3ada0909fe9a5030e15
Kvetoslav1 <kvetko111@gmail.com>
2021-12-02 08:05:30 +0100

    Ver 0.1

--- slideShowScript.js
+++ slideShowScript.js
@@ -1,7 +1,7 @@
 var myIndex = 0;
-carousel();
+slideShow();
 
-function carousel() {
+function slideShow() {
     var i;
     var x = document.getElementsByClassName("slideShow");
     for (i = 0; i < x.length; i++) {
@@ -10,5 +10,5 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 5000);
+    setTimeout(slideShow, 5000);
 }
