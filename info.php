f745fcb8704d19148b69c8039504a2ea3fb65ab6
Kvetoslav1 <kvetko111@gmail.com>
2022-01-17 20:18:08 +0100

    Dorobenie poslednej stránky

--- info.php
+++ info.php
@@ -3,8 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link href="styles/zakladStrankyStyle.css" rel="stylesheet" type="text/css">
+    <link href="styles/tableStyles.css" rel="stylesheet" type="text/css">
+    <link href="styles/gridStyle.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
+<div class="telo">
+
+    <div class="odsadenie">
+
+        <?php require "zakladnaStranka/header.php"; ?>
+
+        <div class="vracenie-vytvaranie">
+            <a class="fa fa-level-down" style="margin-left: 5px;" onclick="history.back()" href="#">Vrátenie späť</a>
+        </div>
+
+        <div id="informacie" class="gridy">
+            <h2 class="header">Informácie o stránke</h2>
+            <p style="grid-column: 1/3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et lacus vitae ex posuere porttitor quis volutpat magna. In hac habitasse platea dictumst. Morbi a massa laoreet, maximus eros vitae, venenatis ligula. Maecenas quis nulla in ante aliquam lobortis. Maecenas iaculis massa et mi rutrum ultrices. Donec accumsan diam et ligula aliquet consequat. Donec sed blandit lectus.
+
+                Donec vel nunc nec arcu tempus vulputate. Mauris vehicula orci eget magna faucibus, non tincidunt risus tincidunt. Sed nec neque at eros placerat volutpat. Morbi sit amet mattis nulla. Quisque sed elit id augue vehicula dapibus. Etiam turpis dui, placerat nec eros sed, congue hendrerit magna. Aenean in molestie orci, a consectetur eros. Ut in mauris nulla. Cras efficitur sem ut felis commodo, et varius neque viverra. Suspendisse sollicitudin, eros et euismod sollicitudin, sapien ante vulputate massa, at ornare lectus augue eu urna. Sed quam sem, imperdiet sed ligula vitae, vulputate euismod enim. Etiam at lorem vestibulum, sollicitudin nulla in, commodo risus. Sed lorem odio, rhoncus non purus et, consectetur efficitur nulla. Curabitur congue finibus sollicitudin. Vivamus nisl metus, tempor ut tempor sed, gravida sed quam. Praesent nec iaculis massa.
+
+                Quisque vel mattis tellus. Vestibulum pulvinar velit id sollicitudin vestibulum. Nam dignissim facilisis risus, ac condimentum nisi laoreet a. Ut iaculis posuere erat eu dapibus. Duis venenatis lobortis pretium. Nunc ut facilisis metus. Fusce iaculis odio at purus efficitur vestibulum. Aliquam mattis ac diam consequat bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut cursus arcu diam, ac efficitur dolor semper nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla quis tellus quis enim convallis tristique.
+
+                Donec vehicula, sem ac molestie condimentum, dolor augue aliquet sem, euismod mattis tellus enim quis diam. Aenean luctus sapien sed pulvinar interdum. Donec ex elit, aliquam ac imperdiet eu, venenatis a ante. Donec sit amet lobortis eros. Fusce a metus a est mollis fringilla. Aliquam fringilla, erat quis dapibus sagittis, tortor est posuere elit, at posuere libero felis placerat dolor. Aenean molestie mattis porttitor. Nunc pretium congue lorem, pretium mollis nisi pretium eget. In sed tellus leo. In euismod, leo tincidunt volutpat malesuada, sem turpis fermentum nunc, et efficitur elit magna eu felis. In ac consectetur erat. Duis et velit ac lectus pretium sodales et sit amet metus. In fermentum lectus nec ipsum egestas viverra. Cras gravida orci consequat leo tincidunt, id condimentum felis fringilla. Phasellus vulputate urna vitae eleifend commodo.
+
+                Praesent malesuada quam ac efficitur vehicula. Sed quis risus dignissim, consequat mauris eget, imperdiet nisi. Vestibulum ligula ipsum, dictum eget nisl cursus, vehicula sodales ipsum. Fusce mi ipsum, viverra id ligula et, eleifend dictum velit. Curabitur hendrerit purus ut finibus lacinia. Maecenas aliquam ornare dolor eget euismod. Sed a lobortis lacus. Nunc et eros tortor. Maecenas in vulputate risus, quis ultricies risus. Duis a mauris a massa tristique dignissim. Vivamus dapibus dui vitae tristique dapibus.
+            </p>
+        </div>
+
+        <footer class="koniec">
+            ©2021 Author: Kvetoslav Varga
+            <p><a href="mailto:kvetko189@gmail.com">kvetko189@gmail.com</a></p>
+        </footer>
+
+    </div>
+
+</div>
 
 </body>
 </html>
